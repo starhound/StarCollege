cab712ba070995c9a84166243f7bd332616882fd
wesley reid <wesleyreid0@gmail.com>
2020-11-01 13:08:45 -0800

    week 3

--- StarCollege/Controllers/HomeController.cs
+++ StarCollege/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using StarCollege.DAL;
 using System.Web.Mvc;
+using StarCollege.ViewModels;
+using System.Linq;
 
 namespace StarCollege.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +15,14 @@ namespace StarCollege.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            System.Linq.IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                               group student by student.EnrollmentDate into dateGroup
+                                                               select new EnrollmentDateGroup()
+                                                               {
+                                                                   EnrollmentDate = dateGroup.Key,
+                                                                   StudentCount = dateGroup.Count()
+                                                               };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +30,12 @@ namespace StarCollege.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
--- StarCollege/Controllers/StudentController.cs
+++ StarCollege/Controllers/StudentController.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using StarCollege.DAL;
 using StarCollege.Models;
+using PagedList;
 
 namespace StarCollege.Controllers
 {
@@ -15,10 +13,54 @@ namespace StarCollege.Controllers
     {
         private SchoolContext db = new SchoolContext();
 
+        /*
         // GET: Student
         public ActionResult Index()
         {
             return View(db.Students.ToList());
+        }*/
+
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var students = from s in db.Students
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+                students = students.Where(s => s.LastName.Contains(searchString) || s.FirstMidName.Contains(searchString));
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Student/Details/5

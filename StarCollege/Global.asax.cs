b6bf47246d8f0a896266452537f3fb172452ab8a
wesley reid <wesleyreid0@gmail.com>
2020-11-28 21:57:47 -0800

    Create interceptor classes

--- StarCollege/Global.asax.cs
+++ StarCollege/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StarCollege.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace StarCollege
 {
@@ -16,6 +14,8 @@ namespace StarCollege
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
         }
     }
 }

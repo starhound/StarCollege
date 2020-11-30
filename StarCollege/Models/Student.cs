056d9ca19c77a6aa431653f5e3e4bb87d46384e6
wesley reid <wesleyreid0@gmail.com>
2020-11-29 19:13:25 -0800

    required

--- StarCollege/Models/Student.cs
+++ StarCollege/Models/Student.cs
@@ -8,15 +8,29 @@ namespace StarCollege.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
         [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
         public string Secret { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
